5d00f8b03c7ca258820d9a9f6810784379c352a5
PeterSpainOXIL <114655461+PeterSpainOXIL@users.noreply.github.com>
2024-10-29 09:59:38 +0000

    more tweaks

--- test/ContentCaching/ContentCacheEvaluatorTest.php
+++ test/ContentCaching/ContentCacheEvaluatorTest.php
@@ -28,20 +28,20 @@ class ContentCacheEvaluatorTest extends \PHPUnit\Framework\TestCase {
         $config = new ContentCacheConfig("1h");
         $cacheEvaluator->cacheResult($config, "http://www.google.com", "MOPALOP");
 
-        $this->assertEquals(["MOPALOP", 3600], $testCache->getCachedItems()["http://www.google.com"]);
+        $this->assertEquals(["MOPALOP", 3600], $testCache->getCachedItems()[md5("http://www.google.com")]);
         $this->assertEquals("MOPALOP", $cacheEvaluator->getCachedResult("http://www.google.com"));
 
         $config = new ContentCacheConfig("2d");
         $cacheEvaluator->cacheResult($config, "http://www.google.co.uk", "MOPALOP");
 
-        $this->assertEquals(["MOPALOP", 3600 * 48], $testCache->getCachedItems()["http://www.google.co.uk"]);
+        $this->assertEquals(["MOPALOP", 3600 * 48], $testCache->getCachedItems()[md5("http://www.google.co.uk")]);
         $this->assertEquals("MOPALOP", $cacheEvaluator->getCachedResult("http://www.google.co.uk"));
 
 
         $config = new ContentCacheConfig("3y");
         $cacheEvaluator->cacheResult($config, "http://www.microsoft.com", "Try a lot");
 
-        $this->assertEquals(["Try a lot", 3600 * 24 * 365 * 3], $testCache->getCachedItems()["http://www.microsoft.com"]);
+        $this->assertEquals(["Try a lot", 3600 * 24 * 365 * 3], $testCache->getCachedItems()[md5("http://www.microsoft.com")]);
         $this->assertEquals("Try a lot", $cacheEvaluator->getCachedResult("http://www.microsoft.com"));
 
 
